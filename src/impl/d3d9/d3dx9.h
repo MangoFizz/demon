ff91330925c0332665bb27652627d19a6c84ef83
MangoFizz <ccs81.mr@gmail.com>
2024-01-28 00:38:25 -0600

    Fix d3dx9 interface definitions to match the actual vtable

--- src/impl/d3d9/d3dx9.h
+++ src/impl/d3d9/d3dx9.h
@@ -3,19 +3,23 @@
 
 #include <d3dx9.h>
 
+#define CAST_VIRTUAL_METHOD(p, i, r) ((r (WINAPI *)())(*(uint32_t *)(((char *)(p)->lpVtbl) + i)))
+#define CALL_VIRTUAL_METHOD(p, i, r, ...) CAST_VIRTUAL_METHOD(p, i, r)(p, __VA_ARGS__)
+#define CALL_VIRTUAL_METHOD_NO_ARGS(p, i, r) CAST_VIRTUAL_METHOD(p, i, r)(p)
+
 /**
  * ID3DXEffect interface helper macros
  */
-#define ID3DXEffect_Release(p) (p)->lpVtbl->Release(p)
-#define ID3DXEffect_GetTechniqueByName(p, n) (p)->lpVtbl->GetTechniqueByName(p, n)
-#define ID3DXEffect_ValidateTechnique(p, t) (p)->lpVtbl->ValidateTechnique(p, t)
-#define ID3DXEffect_SetTechnique(p, t) (p)->lpVtbl->SetTechnique(p, t)
-#define ID3DXEffect_FindNextValidTechnique(p, t, n) (p)->lpVtbl->FindNextValidTechnique(p, t, n)
-#define ID3DXEffect_GetParameterByName(p, s, n) (p)->lpVtbl->GetParameterByName(p, s, n)
+#define ID3DXEffect_Release(p) CALL_VIRTUAL_METHOD_NO_ARGS(p, 0x08, HRESULT)
+#define ID3DXEffect_GetParameterByName(p, s, n) CALL_VIRTUAL_METHOD(p, 0x24, D3DXHANDLE, s, n)
+#define ID3DXEffect_GetTechniqueByName(p, n) CALL_VIRTUAL_METHOD(p, 0x34, D3DXHANDLE, n) 
+#define ID3DXEffect_ValidateTechnique(p, t) CALL_VIRTUAL_METHOD(p, 0xF4, HRESULT, t) 
+#define ID3DXEffect_FindNextValidTechnique(p, t, n) CALL_VIRTUAL_METHOD(p, 0xF8, HRESULT, t, n) 
+#define ID3DXEffect_SetTechnique(p, t) CALL_VIRTUAL_METHOD(p, 0xEC, HRESULT, t)
 
 /**
- * ID3DXBuffer interface helper macros
+ * ID3DXEffectPool interface helper macros
  */
-#define ID3DXBuffer_Release(p) (p)->lpVtbl->Release(p)
+#define ID3DXEffectPool_Release(p) CALL_VIRTUAL_METHOD_NO_ARGS(p, 0x08, HRESULT)
 
 #endif
